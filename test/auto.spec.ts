c1790b71e9d6a212989d0445180e343636208f60
LucBerge <lucas.bergeron@outlook.fr>
2025-03-24 18:03:23 +0100

    WIP - Add jest test

---
+++ test/auto.spec.ts
@@ -0,0 +1,42 @@
+import { CollectorLoader } from '../src/collectors/collectorLoader';
+import { Server } from '../src/server';
+import { describe } from 'node:test';
+import { expect } from '@jest/globals';
+import { AuthenticationError } from '../src/error';
+
+const ids = process.argv.slice(3);
+for (const id of ids) {
+    describe(`${id} tests`, () => {
+        // Load collector
+        CollectorLoader.load(id);
+
+        // Get collector
+        const collector = CollectorLoader.get(id);
+
+        // Check if collector not found
+        if (collector == null) {
+            throw new Error(`No collector with id "${id}" found.`);
+        }
+
+        it('Login with incorrect email format', async () => {
+            const params = {
+                id: 'test',
+                password: 'test'
+            }
+            await expect(collector.collect_new_invoices(params, true, [], Server.DEFAULT_LOCALE, null))
+                .rejects.toThrow(AuthenticationError);
+        });
+
+        /*it('Login with non-existing account', async () => {
+            //TODO
+        });
+
+        it('Login with wrong password', async () => {
+            //TODO
+        });
+
+        it('Login with correct credentials', async () => {
+            //TODO
+        });*/
+    });
+}
