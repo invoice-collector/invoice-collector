fc9bcbb0a2e5cce2203f76f75e44ea1c64828b20
LucBerge <lucas.bergeron@outlook.fr>
2025-03-31 19:11:43 +0200

    Load all collectors if no id provided

--- test/auto.spec.ts
+++ test/auto.spec.ts
@@ -2,15 +2,27 @@ import { expect, describe } from '@jest/globals';
 import { CollectorLoader } from '../src/collectors/collectorLoader';
 import { Server } from '../src/server';
 import { AuthenticationError } from '../src/error';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const ids = process.argv.slice(3);
-for (const id of ids) {
-    describe(`${id} tests`, () => {
+const ONE_MINUTE = 60 * 1000; // 1 minute in milliseconds
+
+// Check if ids are provided
+if (ids.length === 0) {
+    console.info('No ids provided. Loading all the collectors.');
+    CollectorLoader.load();
+}
+else {
+    for (const id of ids) {
         // Load collector
         CollectorLoader.load(id);
+    }
+}
 
-        // Get collector
-        const collector = CollectorLoader.get(id);
+for (const collector of CollectorLoader.getAll()) {
+    const id: string = collector.config.id;
+    describe(`${id} tests`, () => {
 
         // Check if collector not found
         if (collector == null) {
@@ -24,16 +36,16 @@ for (const id of ids) {
             }
             await expect(collector.collect_new_invoices(params, true, [], Server.DEFAULT_LOCALE, null))
                 .rejects.toThrow(AuthenticationError);
-        });
+        }, ONE_MINUTE);
 
         it('Login with non-existing account', async () => {
             const params = {
-                id: 'test@test.test',
+                id: 'fakeemail@test.ic',
                 password: 'test'
             }
             await expect(collector.collect_new_invoices(params, true, [], Server.DEFAULT_LOCALE, null))
                 .rejects.toThrow(AuthenticationError);
-        });
+        }, ONE_MINUTE);
 
         /*it('Login with wrong password', async () => {
             //TODO
