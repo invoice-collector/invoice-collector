5a92d08e7118f4cba6f21bcf2a9de63fddd85628
LucBerge <lucas.bergeron@outlook.fr>
2025-04-01 17:31:19 +0200

    Skip API collectors

--- test/auto.spec.ts
+++ test/auto.spec.ts
@@ -2,6 +2,8 @@ import { expect, describe } from '@jest/globals';
 import { CollectorLoader } from '../src/collectors/collectorLoader';
 import { Server } from '../src/server';
 import { AuthenticationError } from '../src/error';
+import { ApiCollector } from '../src/collectors/apiCollector';
+import { ScrapperCollector } from '../src/collectors/scrapperCollector';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -22,37 +24,38 @@ else {
 
 for (const collector of CollectorLoader.getAll()) {
     const id: string = collector.config.id;
-    describe(`${id} tests`, () => {
+    
+    // Check if collector not found
+    if (collector instanceof ScrapperCollector) {
+        describe(`${id} tests`, () => {
+            it('Login with incorrect email format', async () => {
+                const params = {
+                    id: 'test',
+                    password: 'test'
+                }
+                await expect(collector.collect_new_invoices(params, true, [], Server.DEFAULT_LOCALE, null))
+                    .rejects.toThrow(AuthenticationError);
+            }, ONE_MINUTE);
 
-        // Check if collector not found
-        if (collector == null) {
-            throw new Error(`No collector with id "${id}" found.`);
-        }
+            it('Login with non-existing account', async () => {
+                const params = {
+                    id: 'fakeemail@test.ic',
+                    password: 'test'
+                }
+                await expect(collector.collect_new_invoices(params, true, [], Server.DEFAULT_LOCALE, null))
+                    .rejects.toThrow(AuthenticationError);
+            }, ONE_MINUTE);
 
-        it('Login with incorrect email format', async () => {
-            const params = {
-                id: 'test',
-                password: 'test'
-            }
-            await expect(collector.collect_new_invoices(params, true, [], Server.DEFAULT_LOCALE, null))
-                .rejects.toThrow(AuthenticationError);
-        }, ONE_MINUTE);
+            /*it('Login with wrong password', async () => {
+                //TODO
+            });
 
-        it('Login with non-existing account', async () => {
-            const params = {
-                id: 'fakeemail@test.ic',
-                password: 'test'
-            }
-            await expect(collector.collect_new_invoices(params, true, [], Server.DEFAULT_LOCALE, null))
-                .rejects.toThrow(AuthenticationError);
-        }, ONE_MINUTE);
-
-        /*it('Login with wrong password', async () => {
-            //TODO
+            it('Login with correct credentials', async () => {
+                //TODO
+            });*/
         });
-
-        it('Login with correct credentials', async () => {
-            //TODO
-        });*/
-    });
+    }
+    else {
+        console.info(`Collector ${id} is an API collector. Skipping tests.`);
+    }
 }
