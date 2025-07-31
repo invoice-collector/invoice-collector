ed44bbc24a7fd8eaea2b89f2c6a827a5e2133368
LucBerge <lucas.bergeron@outlook.fr>
2025-07-31 14:05:29 +0200

    Add fields `name` and `features`

--- src/model/plan.ts
+++ src/model/plan.ts
@@ -1,9 +1,10 @@
 export class Plan {
 
-    static TRIAL = new Plan('trial', 1, 1, undefined, undefined, 0, 0, 0, 0);
-    static FREE = new Plan('free', undefined, undefined, undefined, undefined, 0, 0, 0, 0);
+    static TRIAL = new Plan('trial', "Trial", 1, 1, undefined, undefined, 0, 0, 0, 0, []);
+    static FREE = new Plan('free', "Free", undefined, undefined, undefined, undefined, 0, 0, 0, 0, []);
 
     id: string;
+    name: string | undefined;
     maxUsers: number | undefined;
     maxCredentials: number | undefined;
     maxInvoicesPerMonth: number | undefined;
@@ -12,9 +13,11 @@ export class Plan {
     pricePerCredential: number;
     pricePerInvoice: number;
     pricePerCollector: number;
+    features: string[];
 
     constructor(
         id: string,
+        name: string,
         maxUsers: number | undefined,
         maxCredentials: number | undefined,
         maxInvoicesPerMonth: number | undefined,
@@ -22,9 +25,11 @@ export class Plan {
         pricePerUser: number,
         pricePerCredential: number,
         pricePerInvoice: number,
-        pricePerCollector: number
+        pricePerCollector: number,
+        features: string[]
     ) {
         this.id = id;
+        this.name = name;
         this.maxUsers = maxUsers;
         this.maxCredentials = maxCredentials;
         this.maxInvoicesPerMonth = maxInvoicesPerMonth;
@@ -33,5 +38,6 @@ export class Plan {
         this.pricePerCredential = pricePerCredential;
         this.pricePerInvoice = pricePerInvoice;
         this.pricePerCollector = pricePerCollector;
+        this.features = features;
     }
 }
