ad6da7b3ee090301baf2fd3ffc35b8f649e56fd8
LucBerge <lucas.bergeron@outlook.fr>
2025-08-05 19:55:33 +0200

    Add sketch collector `facebook`

---
+++ src/collectors/sketch/facebook/facebook.ts
@@ -0,0 +1,31 @@
+import { SketchCollector } from '../../sketchCollector';
+
+export class FacebookCollector extends SketchCollector {
+
+    static CONFIG = {
+        id: "facebook",
+        name: "Facebook",
+        description: "i18n.collectors.facebook.description",
+        version: "0",
+        website: "https://facebook.com",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/b/b9/2023_Facebook_icon.svg",
+        params: {
+            email: {
+                type: "email",
+                name: "i18n.collectors.all.email",
+                placeholder: "i18n.collectors.all.email.placeholder",
+                mandatory: true
+            },
+            password: {
+                type: "password",
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true
+            }
+        }
+    }
+
+    constructor() {
+        super(FacebookCollector.CONFIG);
+    }
+}
