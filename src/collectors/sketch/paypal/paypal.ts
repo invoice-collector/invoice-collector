265d35e466de9fba27013d7334a50eb712fecfde
LucBerge <lucas.bergeron@outlook.fr>
2025-08-05 19:03:20 +0200

    Add sketch collector `paypal`

---
+++ src/collectors/sketch/paypal/paypal.ts
@@ -0,0 +1,31 @@
+import { SketchCollector } from '../../sketchCollector';
+
+export class PaypalCollector extends SketchCollector {
+
+    static CONFIG = {
+        id: "paypal",
+        name: "Paypal",
+        description: "i18n.collectors.paypal.description",
+        version: "0",
+        website: "https://paypal.com",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/b/b5/PayPal.svg",
+        params: {
+            email: {
+                type: "email",
+                name: "i18n.collectors.all.email",
+                placeholder: "i18n.collectors.all.email.placeholder",
+                mandatory: true
+            },
+            password: {
+                type: "password",
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true
+            }
+        }
+    }
+
+    constructor() {
+        super(PaypalCollector.CONFIG);
+    }
+}
