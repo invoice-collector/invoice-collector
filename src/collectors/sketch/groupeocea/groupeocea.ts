cae6af07ce8aad3f056d753efedb2762345d4991
LucBerge <lucas.bergeron@outlook.fr>
2025-08-06 11:44:02 +0200

    Sketch collector `groupeocea` - Add collector

---
+++ src/collectors/sketch/groupeocea/groupeocea.ts
@@ -0,0 +1,31 @@
+import { SketchCollector } from '../../sketchCollector';
+
+export class GroupeOceaCollector extends SketchCollector {
+
+    static CONFIG = {
+        id: "groupeocea",
+        name: "Groupe OCEA",
+        description: "i18n.collectors.groupeocea.description",
+        version: "0",
+        website: "https://groupe-ocea.fr",
+        logo: "https://www.ecobatiment-cluster.fr/app/uploads/Group-ocea-redimensionne.png",
+        params: {
+            email: {
+                type: "email",
+                name: "i18n.collectors.all.email",
+                placeholder: "i18n.collectors.all.email.placeholder",
+                mandatory: true
+            },
+            password: {
+                type: "password",
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true
+            }
+        }
+    }
+
+    constructor() {
+        super(GroupeOceaCollector.CONFIG);
+    }
+}
