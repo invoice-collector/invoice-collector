f3a8482ad9cdefd27e9baecfcbaef6dc80c78d39
LucBerge <lucas.bergeron@outlook.fr>
2025-08-06 17:57:10 +0200

    Sketch collector `temu` - Add collector

---
+++ src/collectors/sketch/temu/temu.ts
@@ -0,0 +1,31 @@
+import { SketchCollector } from '../../sketchCollector';
+
+export class TemuCollector extends SketchCollector {
+
+    static CONFIG = {
+        id: "temu",
+        name: "Temu",
+        description: "i18n.collectors.temu.description",
+        version: "0",
+        website: "https://temu.com",
+        logo: "https://upload.wikimedia.org/wikipedia/fr/a/a6/Temu_logo.svg",
+        params: {
+            email: {
+                type: "email",
+                name: "i18n.collectors.all.email",
+                placeholder: "i18n.collectors.all.email.placeholder",
+                mandatory: true
+            },
+            password: {
+                type: "password",
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true
+            }
+        }
+    }
+
+    constructor() {
+        super(TemuCollector.CONFIG);
+    }
+}
