a8a6b241520e574ca2e3eb1a71579cf8d7996e11
LucBerge <lucas.bergeron@outlook.fr>
2025-08-06 11:13:59 +0200

    Sketch collector `endesa` - Add collector

---
+++ src/collectors/sketch/endesa/endesa.ts
@@ -0,0 +1,31 @@
+import { SketchCollector } from '../../sketchCollector';
+
+export class EndesaCollector extends SketchCollector {
+
+    static CONFIG = {
+        id: "endesa",
+        name: "Endesa",
+        description: "i18n.collectors.endesa.description",
+        version: "0",
+        website: "https://endesa.fr",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/3/3c/Endesa.svg",
+        params: {
+            email: {
+                type: "email",
+                name: "i18n.collectors.all.email",
+                placeholder: "i18n.collectors.all.email.placeholder",
+                mandatory: true
+            },
+            password: {
+                type: "password",
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true
+            }
+        }
+    }
+
+    constructor() {
+        super(EndesaCollector.CONFIG);
+    }
+}
