9fae3fa70eae2e4cb6a7b90e8f052868250ed391
LucBerge <lucas.bergeron@outlook.fr>
2025-08-06 11:12:28 +0200

    Sketch collector `dalkia` - Add collector

---
+++ src/collectors/sketch/dalkia/dalkia.ts
@@ -0,0 +1,31 @@
+import { SketchCollector } from '../../sketchCollector';
+
+export class DalkiaCollector extends SketchCollector {
+
+    static CONFIG = {
+        id: "dalkia",
+        name: "Dalkia",
+        description: "i18n.collectors.dalkia.description",
+        version: "0",
+        website: "https://dalkia.fr",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/f/f0/Logo_Dalkia.svg",
+        params: {
+            email: {
+                type: "email",
+                name: "i18n.collectors.all.email",
+                placeholder: "i18n.collectors.all.email.placeholder",
+                mandatory: true
+            },
+            password: {
+                type: "password",
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true
+            }
+        }
+    }
+
+    constructor() {
+        super(DalkiaCollector.CONFIG);
+    }
+}
