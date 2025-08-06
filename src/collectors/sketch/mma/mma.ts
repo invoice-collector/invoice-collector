a68fa948f248580a7005fe1c6d53e3487085ff4b
LucBerge <lucas.bergeron@outlook.fr>
2025-08-06 17:34:12 +0200

    Sketch collector `mma` - Add collector

---
+++ src/collectors/sketch/mma/mma.ts
@@ -0,0 +1,31 @@
+import { SketchCollector } from '../../sketchCollector';
+
+export class MmaCollector extends SketchCollector {
+
+    static CONFIG = {
+        id: "mma",
+        name: "Mma",
+        description: "i18n.collectors.mma.description",
+        version: "0",
+        website: "https://mma.fr",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/2/26/Logo_MMA.jpg",
+        params: {
+            email: {
+                type: "email",
+                name: "i18n.collectors.all.email",
+                placeholder: "i18n.collectors.all.email.placeholder",
+                mandatory: true
+            },
+            password: {
+                type: "password",
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true
+            }
+        }
+    }
+
+    constructor() {
+        super(MmaCollector.CONFIG);
+    }
+}
