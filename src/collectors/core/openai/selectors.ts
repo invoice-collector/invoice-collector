d5baf25adfbc150f849a6aa954e7ae23bcf2c053
LucBerge <lucas.bergeron@outlook.fr>
2025-08-08 16:59:33 +0200

    Implement `openai` collector

---
+++ src/collectors/core/openai/selectors.ts
@@ -0,0 +1,58 @@
+export const OpenaiSelectors = {
+    //LOGIN PAGE
+
+    BUTTON_LOGIN: {
+        selector: "main button[data-color='primary'][data-variant='soft']",
+        info: "login button"
+    },
+    FIELD_EMAIL: {
+        selector: "input[autocomplete='email']",
+        info: "login input field"
+    },
+    BUTTON_EMAIL_CONTINUE: {
+        selector: "button[type='submit']",
+        info: "continue button"
+    },
+    CONTAINER_EMAIL_ERROR: {
+        selector: "form ul li",
+        info: "email error container"
+    },
+    FIELD_PASSWORD: {
+        selector: "input[autocomplete='current-password']",
+        info: "password input field"
+    },
+    BUTTON_PASSWORD_CONTINUE: {
+        selector: "button[type='submit']",
+        info: "submit form button"
+    },
+    CONTAINER_PASSWORD_ERROR: {
+        selector: "form ul li",
+        info: "password error container"
+    },
+
+    // 2FA PAGE
+
+    CONTAINER_2FA_INSTRUCTIONS: {
+        selector: "body > div > div > div > div > span",
+        info: "2FA instructions container"
+    },
+    FIELD_2FA_CODE: {
+        selector: "input[autocomplete='one-time-code']",
+        info: "2FA code input field"
+    },
+    BUTTON_2FA_CONTINUE: {
+        selector: "button[type='submit']",
+        info: "2FA code continue button"
+    },
+    CONTAINER_2FA_ERROR: {
+        selector: "form ul li",
+        info: "2FA error container"
+    },
+
+    // CONTAINER INVOICE
+
+    CONTAINER_INVOICE: {
+        selector: "table tbody tr",
+        info: "invoice container"
+    }
+}
