fb8b21fdc036b1de2a5e00afff1fe930248bb829
LucBerge <lucas.bergeron@outlook.fr>
2025-08-11 12:39:14 +0200

    Update test.auto

--- test/auto.spec.ts
+++ test/auto.spec.ts
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
+import promptSync from 'prompt-sync';
+const prompt = promptSync({});
 import { expect, describe } from '@jest/globals';
 import { CollectorLoader } from '../src/collectors/collectorLoader';
 import { AuthenticationError } from '../src/error';
@@ -9,7 +11,8 @@ import { Collect } from '../src/collect/collect';
 import { State } from '../src/model/credential';
 
 const ids = process.argv.slice(3);
-const ONE_MINUTE = 60 * 1000; // 1 minute in milliseconds
+const ONE_MINUTE = 60 * 1000;       // 1 minute in milliseconds
+const TWO_MINUTES = 2 * ONE_MINUTE; // 2 minutes in milliseconds
 
 // Check if ids are provided
 if (ids.length === 0) {
@@ -22,7 +25,6 @@ else {
         CollectorLoader.load(id);
     }
 }
-
 for (const collector of CollectorLoader.getAll()) {
     const id: string = collector.config.id;
 
@@ -32,8 +34,8 @@ for (const collector of CollectorLoader.getAll()) {
             it('Login with incorrect email format', async () => {
                 const secret: Secret = {
                     params: {
-                        id: 'test',
-                        password: 'test'
+                        email: 'incorrect_email_format',
+                        password: 'fake_password'
                     },
                     cookies: null,
                 };
@@ -41,15 +43,15 @@ for (const collector of CollectorLoader.getAll()) {
                 // Collect invoices
                 const collect = new Collect("")
                 collect.state = State.DEFAULT_STATE;
-                await expect(collect.collect_new_invoices(collect.state, collector, secret, true, [], null))
+                await expect(collect.collect_new_invoices(collect.state, collector, secret, false, [], null))
                     .rejects.toThrow(AuthenticationError);
             }, ONE_MINUTE);
 
             it('Login with non-existing account', async () => {
                 const secret: Secret = {
                     params: {
-                        id: 'fakeemail@test.ic',
-                        password: 'test'
+                        email: 'fake@email.com',
+                        password: 'fake_password'
                     },
                     cookies: null,
                 };
@@ -57,20 +59,79 @@ for (const collector of CollectorLoader.getAll()) {
                 // Collect invoices
                 const collect = new Collect("")
                 collect.state = State.DEFAULT_STATE;
-                await expect(collect.collect_new_invoices(collect.state, collector, secret, true, [], null))
+                await expect(collect.collect_new_invoices(collect.state, collector, secret, false, [], null))
                     .rejects.toThrow(AuthenticationError);
             }, ONE_MINUTE);
 
-            /*it('Login with wrong password', async () => {
-                //TODO
-            });
+            it('Login with wrong password', async () => {
+                const secret: Secret = {
+                    params: {
+                        email: 'my@email.com',
+                        password: 'fake_password'
+                    },
+                    cookies: null,
+                };
 
-            it('Login with correct credentials', async () => {
-                //TODO
-            });*/
+                // Collect invoices
+                const collect = new Collect("")
+                collect.state = State.DEFAULT_STATE;
+                await expect(collect.collect_new_invoices(collect.state, collector, secret, false, [], null))
+                    .rejects.toThrow(AuthenticationError);
+            }, ONE_MINUTE);
+
+            let cookies;
+
+            it('Login with correct credentials, no cookies', async () => {
+                const secret: Secret = {
+                    params: {
+                        email: 'my@email.com',
+                        password: 'real_password'
+                    },
+                    cookies: null,
+                };
+
+                // Collect invoices
+                const collect = new Collect("")
+                collect.state = State.DEFAULT_STATE;
+
+                // Define what to do on 2FA
+                collect.twofa_promise.instructions().then((twofa_instruction) => {
+                    const twofa_code = prompt(`${twofa_instruction}: `);
+                    collect.twofa_promise.setCode(twofa_code);
+                });
+
+                await collect.collect_new_invoices(collect.state, collector, secret, false, [], null);
+
+                // Assert cookies are not null
+                expect(secret.cookies).not.toBeNull();
+
+                // Save cookies if collect is successful
+                cookies = secret.cookies;
+            }, TWO_MINUTES);
+
+            // Skip test if cookies are not available = if previous test failed
+            if (cookies) {
+                it('Login with correct credentials and with cookies', async () => {
+                    const secret: Secret = {
+                        params: {
+                            email: 'my@email.com',
+                            password: 'real_password'
+                        },
+                        cookies: cookies
+                    };
+
+                    // Collect invoices
+                    const collect = new Collect("")
+                    collect.state = State.DEFAULT_STATE;
+                    await collect.collect_new_invoices(collect.state, collector, secret, false, [], null);
+                }, TWO_MINUTES);
+            }
+            else {
+                console.info(`Skipping last test for ${id} as cookies are not available.`);
+            }
         });
     }
     else {
-        console.info(`Collector ${id} is an API collector. Skipping tests.`);
+        console.info(`Collector ${id} is not a web collector. Skipping tests.`);
     }
 }
