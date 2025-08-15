321d5864cb9665e1373277b1497502a31a0ed367
LucBerge <lucas.bergeron@outlook.fr>
2025-08-15 20:15:42 +0200

    Bump version

--- src/collectors/core/ovh/ovh.ts
+++ src/collectors/core/ovh/ovh.ts
@@ -11,7 +11,7 @@ export class OvhCollector extends ApiCollector {
         name: "OVH",
         description: "i18n.collectors.ovh.description",
         instructions: "i18n.collectors.ovh.instructions",
-        version: "1",
+        version: "2",
         website: "https://www.ovh.com",
         logo: "https://upload.wikimedia.org/wikipedia/commons/4/45/Logo_OVH.svg",
         params: {
