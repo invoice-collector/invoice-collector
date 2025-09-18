459315832adb3640a8a54d10d3d1fd1b15f8b031
LucBerge <lucas.bergeron@outlook.fr>
2025-09-18 20:00:16 +0200

    CHange name to id in log

--- src/collect/collect.ts
+++ src/collect/collect.ts
@@ -61,7 +61,7 @@ export class Collect {
             if(collector == null) {
                 throw new Error(`No collector with id "${credential.collector_id}" found.`);
             }
-            console.log(`Using collector ${collector.config.name} version ${collector.config.version}`);
+            console.log(`Using collector ${collector.config.id} version ${collector.config.version}`);
 
             // Set collector for twofa promise
             this.twofa_promise.collector = collector;
