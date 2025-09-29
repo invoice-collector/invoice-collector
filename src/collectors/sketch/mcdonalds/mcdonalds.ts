31d30118e832d01a3751fb1b2a5f2bc888994897
LucBerge <lucas.bergeron@outlook.fr>
2025-09-29 15:47:02 +0200

    Add 3 collectors

---
+++ src/collectors/sketch/mcdonalds/mcdonalds.ts
@@ -0,0 +1,33 @@
+
+import { SketchCollector } from '../../sketchCollector';
+
+export class McdonaldsCollector extends SketchCollector {
+
+    static CONFIG = {
+        id: "mcdonalds",
+        name: "McDonald's",
+        description: "i18n.collectors.mcdonalds.description",
+        version: "0",
+        website: "https://www.mcdonalds.fr",
+        logo: "https://upload.wikimedia.org/wikipedia/fr/e/ea/Mcdonalds_France_2009_logo.svg",
+        params: {
+            email: {
+                type: "email",
+                name: "i18n.collectors.all.email",
+                placeholder: "i18n.collectors.all.email.placeholder",
+                mandatory: true
+            },
+            password: {
+                type: "password",
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true
+            }
+        },
+        entryUrl: "https://www.mcdonalds.fr",
+    }
+
+    constructor() {
+        super(McdonaldsCollector.CONFIG);
+    }
+}
