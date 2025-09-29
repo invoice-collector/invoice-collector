0cb939ac50c792fd1180454b89aad3014e902722
LucBerge <lucas.bergeron@outlook.fr>
2025-09-29 13:58:40 +0200

    Add ChatGPT collector

---
+++ src/collectors/sketch/openai_chatgpt/openai_chatgpt.ts
@@ -0,0 +1,33 @@
+
+import { SketchCollector } from '../../sketchCollector';
+
+export class OpenaiChatgptCollector extends SketchCollector {
+
+    static CONFIG = {
+        id: "openai_chatgpt",
+        name: "ChatGPT",
+        description: "i18n.collectors.openai_chatgpt.description",
+        version: "0",
+        website: "https://chatgpt.com",
+        logo: "https://pngimg.com/uploads/chatgpt/chatgpt_PNG2.png",
+        params: {
+            email: {
+                type: "email",
+                name: "i18n.collectors.all.email",
+                placeholder: "i18n.collectors.all.email.placeholder",
+                mandatory: true
+            },
+            password: {
+                type: "password",
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true
+            }
+        },
+        entryUrl: "https://chatgpt.com/#settings/Account",
+    }
+
+    constructor() {
+        super(OpenaiChatgptCollector.CONFIG);
+    }
+}
