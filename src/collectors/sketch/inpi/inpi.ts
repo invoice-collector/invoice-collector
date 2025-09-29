49827f738dc2dfe2d6997f9cf9ffe9642a95b0ec
LucBerge <lucas.bergeron@outlook.fr>
2025-09-29 16:24:50 +0200

    Add inpi collector

---
+++ src/collectors/sketch/inpi/inpi.ts
@@ -0,0 +1,33 @@
+
+import { SketchCollector } from '../../sketchCollector';
+
+export class InpiCollector extends SketchCollector {
+
+    static CONFIG = {
+        id: "inpi",
+        name: "Inpi",
+        description: "i18n.collectors.inpi.description",
+        version: "0",
+        website: "https://www.inpi.fr/",
+        logo: "https://upload.wikimedia.org/wikipedia/fr/a/ab/Logo-inpi.svg",
+        params: {
+            email: {
+                type: "email",
+                name: "i18n.collectors.all.email",
+                placeholder: "i18n.collectors.all.email.placeholder",
+                mandatory: true
+            },
+            password: {
+                type: "password",
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true
+            }
+        },
+        entryUrl: "https://www.inpi.fr/",
+    }
+
+    constructor() {
+        super(InpiCollector.CONFIG);
+    }
+}
