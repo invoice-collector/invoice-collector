957433fe8524a99992431b0d723a5ecf0f370f3c
LucBerge <lucas.bergeron@outlook.fr>
2025-10-14 09:55:07 +0200

    Add 5 sketch collectors

---
+++ src/collectors/sketch/dedar_com/dedar_com.ts
@@ -0,0 +1,36 @@
+
+import { SketchCollector } from '../../sketchCollector';
+import { CollectorState, CollectorType } from '../../abstractCollector';
+
+export class DedarComCollector extends SketchCollector {
+
+    static CONFIG = {
+        id: "dedar_com",
+        name: "Dedar",
+        description: "i18n.collectors.dedar_com.description",
+        version: "0",
+        website: "https://dedar.com/",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe-2Jwj3kDWx3z-Nkibff0VQpSuWSdehProYEydPe38IyG4rDL_FlwNEMRe91lBott71E&usqp=CAU",
+        type: CollectorType.SKETCH,
+        params: {
+            email: {
+                type: "email",
+                name: "i18n.collectors.all.email",
+                placeholder: "i18n.collectors.all.email.placeholder",
+                mandatory: true
+            },
+            password: {
+                type: "password",
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true
+            }
+        },
+        entryUrl: "https://dedar.com/login.php",
+        state: CollectorState.DEVELOPMENT
+    }
+
+    constructor() {
+        super(DedarComCollector.CONFIG);
+    }
+}
