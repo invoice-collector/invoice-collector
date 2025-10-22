01595f7949a4ca0f4e53cc8cdb2229cef1d4245a
LucBerge <lucas.bergeron@outlook.fr>
2025-10-22 15:42:26 +0200

    Add collector `radiusfuel`

---
+++ src/collectors/sketch/radiusfuel/radiusfuel.ts
@@ -0,0 +1,34 @@
+import { SketchCollector } from '../../sketchCollector';
+import { CollectorState, CollectorType } from '../../abstractCollector';
+
+export class RadiusFuelCollector extends SketchCollector {
+
+    static CONFIG = {
+        id: "radiusfuel",
+        name: "Radius Fuel",
+        description: "i18n.collectors.radiusfuel.description",
+        version: "0",
+        website: "https://www.radiusfuelsolutions.com",
+        logo: "https://www.adamphones.com/wp-content/uploads/2020/02/radius-products-radius-fuel-solutions.png",
+        type: CollectorType.SKETCH,
+        params: {
+            email: {
+                type: "email",
+                name: "i18n.collectors.all.email",
+                placeholder: "i18n.collectors.all.email.placeholder",
+                mandatory: true
+            },
+            password: {
+                type: "password",
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true
+            }
+        },
+        state: CollectorState.DEVELOPMENT
+    }
+
+    constructor() {
+        super(RadiusFuelCollector.CONFIG);
+    }
+}
