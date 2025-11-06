34ed5fbf0454f53cff3690240e991edc14efb65a
LucBerge <lucas.bergeron@outlook.fr>
2025-11-06 10:29:01 +0100

    Add collector `mgf_fr`

---
+++ src/collectors/sketch/mgf_fr/mgf_fr.ts
@@ -0,0 +1,34 @@
+import { SketchCollector } from '../../sketchCollector';
+import { CollectorState, CollectorType } from '../../abstractCollector';
+
+export class MgfCollector extends SketchCollector {
+
+    static CONFIG = {
+        id: "mgf_fr",
+        name: "MGF",
+        description: "i18n.collectors.mgf_fr.description",
+        version: "0",
+        website: "https://www.mgf-info.fr",
+        logo: "https://www.mgf-info.fr/pub/media/logo/websites/1/MGF_133x60.png",
+        type: CollectorType.SKETCH,
+        params: {
+            email: {
+                type: "email",
+                name: "i18n.collectors.all.email",
+                placeholder: "i18n.collectors.all.email.placeholder",
+                mandatory: true
+            },
+            password: {
+                type: "password",
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true
+            }
+        },
+        state: CollectorState.DEVELOPMENT
+    }
+
+    constructor() {
+        super(MgfCollector.CONFIG);
+    }
+}
