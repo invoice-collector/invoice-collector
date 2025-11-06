93080f3a62eef6470de7716c83316eccd60e2b60
LucBerge <lucas.bergeron@outlook.fr>
2025-11-06 10:54:13 +0100

    Add collector `idnumerique`

---
+++ src/collectors/sketch/idnumerique/idnumerique.ts
@@ -0,0 +1,34 @@
+import { SketchCollector } from '../../sketchCollector';
+import { CollectorState, CollectorType } from '../../abstractCollector';
+
+export class IdNumeriqueCollector extends SketchCollector {
+
+    static CONFIG = {
+        id: "idnumerique",
+        name: "Id numerique",
+        description: "i18n.collectors.idnumerique.description",
+        version: "0",
+        website: "https://www.idnumerique.fr",
+        logo: "https://www.idnumerique.fr/wp-content/uploads/2014/06/logo-header.png",
+        type: CollectorType.SKETCH,
+        params: {
+            email: {
+                type: "email",
+                name: "i18n.collectors.all.email",
+                placeholder: "i18n.collectors.all.email.placeholder",
+                mandatory: true
+            },
+            password: {
+                type: "password",
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true
+            }
+        },
+        state: CollectorState.DEVELOPMENT
+    }
+
+    constructor() {
+        super(IdNumeriqueCollector.CONFIG);
+    }
+}
