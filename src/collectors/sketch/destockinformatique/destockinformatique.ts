92555afc1601b2ce99ca722121e2dd06a3d52204
LucBerge <lucas.bergeron@outlook.fr>
2025-11-06 10:31:23 +0100

    Add collector `destockinformatique`

---
+++ src/collectors/sketch/destockinformatique/destockinformatique.ts
@@ -0,0 +1,34 @@
+import { SketchCollector } from '../../sketchCollector';
+import { CollectorState, CollectorType } from '../../abstractCollector';
+
+export class DestockInformatiqueCollector extends SketchCollector {
+
+    static CONFIG = {
+        id: "destockinformatique",
+        name: "Destock Informatique",
+        description: "i18n.collectors.destockinformatique.description",
+        version: "0",
+        website: "https://www.destock-informatique.com",
+        logo: "https://media.destock-informatique.com/img/destock-informatique-logo-1577650106.jpg",
+        type: CollectorType.SKETCH,
+        params: {
+            email: {
+                type: "email",
+                name: "i18n.collectors.all.email",
+                placeholder: "i18n.collectors.all.email.placeholder",
+                mandatory: true
+            },
+            password: {
+                type: "password",
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true
+            }
+        },
+        state: CollectorState.DEVELOPMENT
+    }
+
+    constructor() {
+        super(DestockInformatiqueCollector.CONFIG);
+    }
+}
