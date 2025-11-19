750fbb2244608b32e63e57afc1a80f89fcc24204
LucBerge <lucas.bergeron@outlook.fr>
2025-11-19 10:05:38 +0100

    Add collector `bolia`

---
+++ src/collectors/sketch/bolia/bolia.ts
@@ -0,0 +1,34 @@
+import { SketchCollector } from '../../sketchCollector';
+import { CollectorState, CollectorType } from '../../abstractCollector';
+
+export class BoliaCollector extends SketchCollector {
+
+    static CONFIG = {
+        id: "bolia",
+        name: "Bolia",
+        description: "i18n.collectors.bolia.description",
+        version: "0",
+        website: "https://www.bolia.com",
+        logo: "https://iconlogovector.com/uploads/images/2025/05/lg-68310146ab917-Bolia.webp",
+        type: CollectorType.SKETCH,
+        params: {
+            email: {
+                type: "email",
+                name: "i18n.collectors.all.email",
+                placeholder: "i18n.collectors.all.email.placeholder",
+                mandatory: true
+            },
+            password: {
+                type: "password",
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true
+            }
+        },
+        state: CollectorState.DEVELOPMENT
+    }
+
+    constructor() {
+        super(BoliaCollector.CONFIG);
+    }
+}
