c1d5596176a09995ee755e551b5404b7ad45031a
LucBerge <lucas.bergeron@outlook.fr>
2025-11-19 10:15:29 +0100

    Add collector `apple_icloud`

---
+++ src/collectors/sketch/apple_icloud/apple_icloud.ts
@@ -0,0 +1,34 @@
+import { SketchCollector } from '../../sketchCollector';
+import { CollectorState, CollectorType } from '../../abstractCollector';
+
+export class AppleIcloudCollector extends SketchCollector {
+
+    static CONFIG = {
+        id: "apple_icloud",
+        name: "Apple iCloud",
+        description: "i18n.collectors.apple_icloud.description",
+        version: "0",
+        website: "https://www.icloud.com",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/1/1c/ICloud_logo.svg",
+        type: CollectorType.SKETCH,
+        params: {
+            email: {
+                type: "email",
+                name: "i18n.collectors.all.email",
+                placeholder: "i18n.collectors.all.email.placeholder",
+                mandatory: true
+            },
+            password: {
+                type: "password",
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true
+            }
+        },
+        state: CollectorState.DEVELOPMENT
+    }
+
+    constructor() {
+        super(AppleIcloudCollector.CONFIG);
+    }
+}
