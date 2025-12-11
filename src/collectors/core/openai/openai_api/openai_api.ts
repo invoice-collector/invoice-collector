7abb7e9a016fc437fed82d878e94b80c7506e296
LucBerge <lucas.bergeron@outlook.fr>
2025-12-11 19:06:37 +0100

    Add mfa-challenge url

--- src/collectors/core/openai/openai_api/openai_api.ts
+++ src/collectors/core/openai/openai_api/openai_api.ts
@@ -10,7 +10,7 @@ export class OpenaiApiCollector extends OpenaiCommonCollector {
         id: "openai_api",
         name: "OpenAI (API)",
         description: "i18n.collectors.openai_api.description",
-        version: "10",
+        version: "11",
         website: "https://openai.com",
         logo: "https://upload.wikimedia.org/wikipedia/commons/4/4d/OpenAI_Logo.svg",
         type: CollectorType.WEB,
