c5f6440af26f0fedca46fc8fd22071b3065f3532
LucBerge <lucas.bergeron@outlook.fr>
2025-12-28 13:48:30 +0100

    Implement microsoft oauth login method

--- src/collectors/oauth2/microsoftOauth2.ts
+++ src/collectors/oauth2/microsoftOauth2.ts
@@ -3,20 +3,54 @@ import { Driver } from "../../driver/driver";
 import { WebSocketServer } from "../../websocket/webSocketServer";
 
 export const MicrosoftOauth2Selectors = {
+    // LOGIN
+
+    BUTTON_PASSWORD_AUTHENTICATION_METHOD: {
+        selector: "div[data-testid='tileList']> div[role='listitem']:nth-of-type(2)",
+        info: "Password authentication method button"
+    },
+    INPUT_PASSWORD: {
+        selector: "#passwordEntry",
+        info: "Password input field"
+    },
+    BUTTON_SUBMIT_PASSWORD: {
+        selector: "button[type='submit']",
+        info: "Submit password button"
+    },
+    CONTAINER_PASSWORD_ERROR: {
+        selector: "div[id*='validationMessage'][role='alert']",
+        info: "Password error message container"
+    },
+
+    // TWOFA
+
     BUTTON_SEND_NOTIFICATION: {
         selector: "button[type='submit'][data-testid='primaryButton']",
-        type: "Button send notification"
+        info: "Button send notification"
     },
     CONTAINERS_2FA_INSTRUCTIONS: {
         selector: "h1[data-testid='title'], div[data-testid='displaySign'], div[data-testid='description']",
-        type: "2FA instructions container"
+        info: "2FA instructions container"
     }
 }
 
 export class MicrosoftOauth2 {
 
     static async login(driver: Driver, params: any, webSocketServer: WebSocketServer | undefined): Promise<string | void> {
-        //TODO
+        if(driver.url().includes("login.live.com/oauth2")) {
+            // Select password authentication method if displayed
+            const passwordAuthMethod = await driver.getElement(MicrosoftOauth2Selectors.BUTTON_PASSWORD_AUTHENTICATION_METHOD, {timeout: 0});
+            if(passwordAuthMethod)
+                await driver.leftClick(MicrosoftOauth2Selectors.BUTTON_PASSWORD_AUTHENTICATION_METHOD, {navigation: false});
+                await driver.inputText(MicrosoftOauth2Selectors.INPUT_PASSWORD, params.password);
+                await driver.leftClick(MicrosoftOauth2Selectors.BUTTON_SUBMIT_PASSWORD);
+
+                // Check if password is incorrect
+                const twofa_alert = await driver.getElement(MicrosoftOauth2Selectors.CONTAINER_PASSWORD_ERROR, { raiseException: false, timeout: 2000 });
+                if (twofa_alert) {
+                    return await twofa_alert.textContent("i18n.collectors.all.password.error");
+                }
+        }
     }
 
     static async needTwofa(driver: Driver): Promise<string | void> {
