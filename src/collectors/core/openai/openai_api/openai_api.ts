cda1b54f3378541f39f2db962bab25ed7a4369ff
LucBerge <lucas.bergeron@outlook.fr>
2026-01-02 16:07:17 +0100

    Do not wait for navigation

--- src/collectors/core/openai/openai_api/openai_api.ts
+++ src/collectors/core/openai/openai_api/openai_api.ts
@@ -10,7 +10,7 @@ export class OpenaiApiCollector extends OpenaiCommonCollector {
         id: "openai_api",
         name: "OpenAI (API)",
         description: "i18n.collectors.openai_api.description",
-        version: "13",
+        version: "14",
         website: "https://openai.com",
         logo: "https://upload.wikimedia.org/wikipedia/commons/4/4d/OpenAI_Logo.svg",
         type: CollectorType.WEB,
