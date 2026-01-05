6497b382160f05081a31b0060804dd59c4aace51
LucBerge <lucas.bergeron@outlook.fr>
2026-01-05 17:48:30 +0100

    Add collector agent `wurth_fr`

---
+++ src/collectors/sketch/wurth_at/wurth_at.ts
@@ -0,0 +1,42 @@
+
+import { SketchCollector } from '../../sketchCollector';
+import { CollectorState, CollectorType } from '../../abstractCollector';
+
+export class WurthAtCollector extends SketchCollector {
+
+    static CONFIG = {
+        id: "wurth_at",
+        name: "Wurth (.at)",
+        description: "i18n.collectors.wurth.description",
+        version: "1",
+        website: "https://eshop.wuerth.at",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/7/7e/W%C3%BCrth_logo.svg",
+        type: CollectorType.SKETCH,
+        params: {
+            client_id: {
+                type: "string",
+                name: "i18n.collectors.wurth.client_id",
+                placeholder: "i18n.collectors.wurth.client_id.placeholder",
+                mandatory: true
+            },
+            login: {
+                type: "string",
+                name: "i18n.collectors.wurth.login",
+                placeholder: "i18n.collectors.wurth.login.placeholder",
+                mandatory: true
+            },
+            password: {
+                type: "password",
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true
+            }
+        },
+        entryUrl: "https://eshop.wuerth.at",
+        state: CollectorState.DEVELOPMENT
+    }
+
+    constructor() {
+        super(WurthAtCollector.CONFIG);
+    }
+}
---
+++ src/collectors/sketch/wurth_de/wurth_de.ts
@@ -0,0 +1,42 @@
+
+import { SketchCollector } from '../../sketchCollector';
+import { CollectorState, CollectorType } from '../../abstractCollector';
+
+export class WurthDeCollector extends SketchCollector {
+
+    static CONFIG = {
+        id: "wurth_de",
+        name: "Wurth (.de)",
+        description: "i18n.collectors.wurth.description",
+        version: "1",
+        website: "https://eshop.wuerth.de",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/7/7e/W%C3%BCrth_logo.svg",
+        type: CollectorType.SKETCH,
+        params: {
+            client_id: {
+                type: "string",
+                name: "i18n.collectors.wurth.client_id",
+                placeholder: "i18n.collectors.wurth.client_id.placeholder",
+                mandatory: true
+            },
+            login: {
+                type: "string",
+                name: "i18n.collectors.wurth.login",
+                placeholder: "i18n.collectors.wurth.login.placeholder",
+                mandatory: true
+            },
+            password: {
+                type: "password",
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true
+            }
+        },
+        entryUrl: "https://eshop.wuerth.de/de/DE/EUR/",
+        state: CollectorState.DEVELOPMENT
+    }
+
+    constructor() {
+        super(WurthDeCollector.CONFIG);
+    }
+}
