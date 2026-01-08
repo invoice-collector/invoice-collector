1186cb49ae6d7ed4e366c509cd2299c4ab84d647
LucBerge <lucas.bergeron@outlook.fr>
2026-01-08 20:29:44 +0100

    Implement mandatory methods

--- src/collectors/sketchCollector.ts
+++ src/collectors/sketchCollector.ts
@@ -1,6 +1,8 @@
-import { CollectorType, CollectorState } from "./abstractCollector";
+import { CollectorType, CollectorState, Invoice } from "./abstractCollector";
 import { WebCollector, WebConfig } from "./webCollector";
 import { Driver, Element } from "../driver/driver";
+import { WebSocketServer } from "../websocket/webSocketServer";
+import { AuthenticationError } from "../error";
 
 export type SketchConfig = WebConfig & {
 }
@@ -16,8 +18,20 @@ export abstract class SketchCollector extends WebCollector {
         });
     }
 
+    async login(driver: Driver, params: any, webSocketServer: WebSocketServer | undefined ): Promise<string | void> {
+        throw new AuthenticationError("This collector is available with interactive login only.", this);
+    }
+
     async getInvoices(driver: Driver, params: any): Promise<Element[]> {
         return [];
+    }
+
+    async data(driver: Driver, params: any, element: Element): Promise<Invoice | null> {
+        throw new Error("Method not implemented.");
+    }
+
+    async download(driver: Driver, params: any, element: Element, invoice: Invoice): Promise<string[]> {
+        throw new Error("Method not implemented.");
     }
 
     async _close(): Promise<void> {
