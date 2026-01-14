6ab41d925a6dd7dde1e8b87bf80668a47c82f245
LucBerge <lucas.bergeron@outlook.fr>
2026-01-14 11:00:56 +0100

    Update collector selectors

--- src/collectors/core/leclerc/leclerc.ts
+++ src/collectors/core/leclerc/leclerc.ts
@@ -1,20 +1,19 @@
-import { Driver, Element } from '../../../driver/driver';
-import { UnfinishedCollectorError } from '../../../error';
+import { Driver } from '../../../driver/driver';
+import { AgentCollector } from '../../../premium/collectors/customAgentCollector';
 import { WebSocketServer } from '../../../websocket/webSocketServer';
-import { CollectorCaptcha, CollectorState, CollectorType, Invoice } from '../../abstractCollector';
-import { WebCollector } from '../../webCollector';
+import { CollectorCaptcha, CollectorState, CollectorType } from '../../abstractCollector';
 import { LeclercSelectors } from './selectors';
 
-export class LeclercCollector extends WebCollector {
+export class LeclercCollector extends AgentCollector {
 
     static CONFIG = {
         id: "leclerc",
         name: "E.Leclerc",
         description: "i18n.collectors.leclerc.description",
-        version: "8",
+        version: "9",
         website: "https://www.e.leclerc/",
         logo: "https://upload.wikimedia.org/wikipedia/commons/e/ed/Logo_E.Leclerc_Sans_le_texte.svg",
-        type: CollectorType.WEB,
+        type: CollectorType.AGENT,
         params: {
             id: {
                 type: "string",
@@ -41,51 +40,34 @@ export class LeclercCollector extends WebCollector {
     }
 
     async login(driver: Driver, params: any, webSocketServer: WebSocketServer | undefined): Promise<string | void> {
-            // Accept cookies
-            await driver.leftClick(LeclercSelectors.BUTTON_ACCEPT_COOKIES, { raiseException: false, timeout: 5000});
+        // Accept cookies
+        await driver.leftClick(LeclercSelectors.BUTTON_ACCEPT_COOKIES, { raiseException: false, timeout: 5000, navigation: false });
 
-            // Input email
-            await driver.inputText(LeclercSelectors.FIELD_EMAIL, params.id);
-            await driver.leftClick(LeclercSelectors.BUTTON_LOGIN);
-    
-            // Check if email is incorrect
-            const email_error = await driver.getElement(LeclercSelectors.CONTAINER_EMAIL_ERROR, { raiseException: false, timeout: 2000 });
-            if (email_error) {
-                return await email_error.textContent("i18n.collectors.all.email.error");
-            }
+        // Input email
+        await driver.inputText(LeclercSelectors.FIELD_EMAIL, params.id);
+        await driver.leftClick(LeclercSelectors.BUTTON_CONTINUE, {navigation: false});
 
-            // Check if email is incorrect
-            const container_signup = await driver.getElement(LeclercSelectors.CONTAINER_SIGNUP, { raiseException: false, timeout: 2000 });
-            if (container_signup) {
-                return "i18n.collectors.all.signup.error"
-            }
-    
-            // Input password
-            await driver.inputText(LeclercSelectors.FIELD_PASSWORD, params.password);
-            await driver.leftClick(LeclercSelectors.BUTTON_LOGIN);
-    
-            // Check if password is incorrect
-            const password_error = await driver.getElement(LeclercSelectors.CONTAINER_PASSWORD_ERROR, { raiseException: false, timeout: 5000 });
-            if (password_error) {
-                return await password_error.textContent("i18n.collectors.all.password.error");
-            }
-    }
+        // Check if email is incorrect
+        const email_error = await driver.getElement(LeclercSelectors.CONTAINER_EMAIL_ERROR, { raiseException: false, timeout: 2000 });
+        if (email_error) {
+            return await email_error.textContent("i18n.collectors.all.email.error");
+        }
 
-    async navigate(driver: Driver, params: any): Promise<void> {
-        throw new UnfinishedCollectorError(this);
-        //await driver.goto(this.config.entryUrl);
-        // TODO : Implement the rest of the collector
-    }
-     
-    async getInvoices(driver: Driver, params: any): Promise<Element[]> {
-        throw new UnfinishedCollectorError(this);
-    }
+        // Check if email is incorrect
+        const container_signup = await driver.getElement(LeclercSelectors.CONTAINER_SIGNUP, { raiseException: false, timeout: 2000 });
+        if (container_signup) {
+            return "i18n.collectors.all.signup.error"
+        }
 
-    async data(driver: Driver, params: any, element: Element): Promise<Invoice | null> {
-        throw new UnfinishedCollectorError(this);
-    }
+        // Input password
+        await driver.inputText(LeclercSelectors.FIELD_PASSWORD, params.password);
+        await driver.leftClick(LeclercSelectors.CHECKBOX_STAY_CONNECTED, {navigation: false});
+        await driver.leftClick(LeclercSelectors.BUTTON_LOGIN);
 
-    async download(driver: Driver, params: any, element: Element, invoice: Invoice): Promise<string[]> {
-        throw new UnfinishedCollectorError(this);
+        // Check if password is incorrect
+        const password_error = await driver.getElement(LeclercSelectors.CONTAINER_PASSWORD_ERROR, { raiseException: false, timeout: 5000 });
+        if (password_error) {
+            return await password_error.textContent("i18n.collectors.all.password.error");
+        }
     }
 }
