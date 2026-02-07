c3d0c1dea31ecc92b82b9402ebade1cc07ca883d
LucBerge <lucas.bergeron@outlook.fr>
2026-02-07 20:16:07 +0100

    Do not catch

--- src/callback/callback.ts
+++ src/callback/callback.ts
@@ -53,21 +53,17 @@ export class CallbackHandler {
     async sendNotificationDisconnected(collector_id: string, credential_id: string,  user_id: string, remote_id: string): Promise<void> {
         console.log(`Sending disconnected notification to callback ${this.callback} for credential ${credential_id}`);
         if (this.callback) {
-            try {
-                await this.sendRequest({
-                    type: "notification_disconnected",
-                    collector: collector_id,
-                    credential_id: credential_id,
-                    user_id: user_id,
-                    remote_id: remote_id
-                });
-                console.log(`Callback ${this.callback} successfully reached, disconnected notification sent`);
-            } catch (error) {
-                console.error(`Failed to send disconnected notification to callback ${this.callback}:`, error);
-            }
+            await this.sendRequest({
+                type: "notification_disconnected",
+                collector: collector_id,
+                credential_id: credential_id,
+                user_id: user_id,
+                remote_id: remote_id
+            });
+            console.log(`Callback ${this.callback} successfully reached, disconnected notification sent`);
         }
         else {
-            console.error("Callback URL not defined by customer, skipping disconnected notification request");
+            throw new Error("Callback URL not defined by customer, skipping disconnected notification request");
         }
     }
 }
