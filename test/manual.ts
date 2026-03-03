9eec8fd345cb089e4681e389ec50d7083c0e5d5e
LucBerge <lucas.bergeron@outlook.fr>
2026-03-03 17:01:57 +0100

    Fix bug - Do not close rl

--- test/manual.ts
+++ test/manual.ts
@@ -207,8 +207,6 @@ function getHashFromSecret(secret: Secret): string {
                     rl.on('line', (input) => {
                         // Send close message to server
                         webSocketClient.send(JSON.stringify({ type: 'interactive', reason: 'close' }));
-                        // Close readline interface
-                        rl.close();
                     });
                 }
                 else if(parsedData.type == "state" && parsedData.state.index == 3) {
