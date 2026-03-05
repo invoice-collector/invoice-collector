e1e4b6c8fa360c9fd05210a58eaedc0439276350
LucBerge <lucas.bergeron@outlook.fr>
2026-03-05 18:44:42 +0100

    Add openapi file as static file

--- devscripts/build_openapi.js
+++ devscripts/build_openapi.js
@@ -7,7 +7,7 @@ const { SWAGGER_DEFINITION } = require('./swaggerConfig.js');
 
 const spec = swaggerJsdoc(SWAGGER_DEFINITION);
 const yamlStr = YAML.stringify(spec);
-const outputPath = path.join(__dirname, 'openapi.yml');
+const outputPath = path.join(__dirname, '..', 'views', 'openapi', 'openapi.yml');
 
 fs.writeFileSync(outputPath, yamlStr, 'utf8');
 console.log(`OpenAPI spec written to ${outputPath}`);
