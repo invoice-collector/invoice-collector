6392165dd06f14c8e73a15b67e141bf48b9dcfd4
LucBerge <lucas.bergeron@outlook.fr>
2026-03-23 17:03:59 +0100

    Update selector, bump version

--- src/collectors/core/bureau_vallee/bureau_vallee.ts
+++ src/collectors/core/bureau_vallee/bureau_vallee.ts
@@ -11,7 +11,7 @@ export class BureauValleeCollector extends WebCollector {
         id: "bureau_vallee",
         name: "Bureau Vallée",
         description: "i18n.collectors.bureau_vallee.description",
-        version: "13",
+        version: "14",
         website: "https://www.bureau-vallee.fr",
         logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Logo-bureau-vallee-2021.png/320px-Logo-bureau-vallee-2021.png",
         type: CollectorType.WEB,
