3523750b04e219cfcb6c5f416900c58d23019745
LucBerge <lucas.bergeron@outlook.fr>
2026-04-22 12:27:07 +0200

    Improve Google Oauth2

--- src/collectors/oauth2/googleOauth2.ts
+++ src/collectors/oauth2/googleOauth2.ts
@@ -18,6 +18,18 @@ export const GoogleOauth2Selectors = {
         selector: "div:has(> div > div >div > div > input[type='email']) > div[aria-live='polite'] > div:has(> span)",
         type: "Button next"
     },
+    BUTTON_TRY_ANOTHER_WAY: {
+        selector: "main > div[data-secondary-action-label]:not(:has([data-primary-action-label])) button",
+        type: "Button try another way"
+    },
+    BUTTON_TRY_ANOTHER_METHOD: {
+        selector: "main > div[data-secondary-action-label] > div > div:nth-of-type(2) > div > div > button",
+        type: "Button try another method"
+    },
+    BUTTON_PASSWORD_METHOD: {
+        selector: "section ul > li > div[data-challengetype='1']",
+        type: "Button password method"
+    },
     INPUT_PASSWORD: {
         selector: "input[type='password']",
         type: "Input password"
@@ -64,6 +76,20 @@ export class GoogleOauth2 {
             const emailError = await driver.getElement(GoogleOauth2Selectors.CONTAINER_EMAIL_ERROR, { raiseException: false, timeout: 1000 });
             if (emailError) {
                 return await emailError.textContent("i18n.collectors.all.email.error");
+            }
+
+            // Wait for password input
+            const inputPassword = await driver.getElement(GoogleOauth2Selectors.INPUT_PASSWORD, { raiseException: false, timeout: 5000 });
+            // If not password displayed
+            if(!inputPassword) {
+                // Click try another way if displayed
+                const tryAnotherWayElement = await driver.leftClick(GoogleOauth2Selectors.BUTTON_TRY_ANOTHER_WAY, { raiseException: false, timeout: 100 });
+                // Click try another method if displayed
+                const tryAnotherMethodElement = await driver.leftClick(GoogleOauth2Selectors.BUTTON_TRY_ANOTHER_METHOD, { raiseException: false, timeout: 100 });
+                if(tryAnotherWayElement || tryAnotherMethodElement) {
+                    // Select password method
+                    await driver.leftClick(GoogleOauth2Selectors.BUTTON_PASSWORD_METHOD);
+                }
             }
 
             // Input password and click next
