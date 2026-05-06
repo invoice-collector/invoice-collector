fedf2641272e78a1fd9a9f3ac5a131d345cf569e
LucBerge <lucas.bergeron@outlook.fr>
2026-05-06 12:04:08 +0200

    Update microsoft oauth2

--- src/collectors/oauth2/microsoftOauth2.ts
+++ src/collectors/oauth2/microsoftOauth2.ts
@@ -5,9 +5,17 @@ import { WebSocketServer } from "../../websocket/webSocketServer";
 export const MicrosoftOauth2Selectors = {
     // LOGIN
 
+    BUTTON_DISPLAY_MORE_OPTIONS: {
+        selector: "#view > div > div:not(:has(div[role='listitem'])) > button",
+        info: "Display more authentication options button"
+    },
     BUTTON_PASSWORD_AUTHENTICATION_METHOD: {
-        selector: "div[data-testid='tileList']> div[role='listitem']:nth-of-type(2)",
+        selector: "#view > div > div > div[role='listitem'] svg path[d*='M11.78 10.22a']",
         info: "Password authentication method button"
+    },
+    BUTTON_PHONE_OTP_METHOD: {
+        selector: "#view > div > div > div[role='listitem'] > div[aria-label*='******']:not([aria-label*='@'])",
+        info: "Phone OTP method button"
     },
     INPUT_PASSWORD: {
         selector: "#passwordEntry",
@@ -20,6 +28,10 @@ export const MicrosoftOauth2Selectors = {
     CONTAINER_PASSWORD_ERROR: {
         selector: "div[id*='validationMessage'][role='alert']",
         info: "Password error message container"
+    },
+    BUTTON_STAY_CONNECTED: {
+        selector: "#view > div > div > button[data-testid='primaryButton']",
+        info: "Stay connected checkbox"
     },
 
     // TWOFA
@@ -42,8 +54,11 @@ export class MicrosoftOauth2 {
 
     static async login(driver: Driver, params: any, webSocketServer: WebSocketServer | undefined): Promise<string | void> {
         if(MicrosoftOauth2.check(driver)) {
+            // Display more options if button is displayed
+            await driver.leftClick(MicrosoftOauth2Selectors.BUTTON_DISPLAY_MORE_OPTIONS, { timeout: 1000, raiseException: false });
+
             // Select password authentication method if displayed
-            const passwordAuthMethod = await driver.getElement(MicrosoftOauth2Selectors.BUTTON_PASSWORD_AUTHENTICATION_METHOD, { timeout: 1000, raiseException: false });
+            const passwordAuthMethod = await driver.getElement(MicrosoftOauth2Selectors.BUTTON_PASSWORD_AUTHENTICATION_METHOD, { timeout: 100, raiseException: false });
             if(passwordAuthMethod) {
                 await driver.leftClick(MicrosoftOauth2Selectors.BUTTON_PASSWORD_AUTHENTICATION_METHOD, {navigation: false});
                 await driver.inputText(MicrosoftOauth2Selectors.INPUT_PASSWORD, params.password);
@@ -54,18 +69,29 @@ export class MicrosoftOauth2 {
                 if (twofa_alert) {
                     return await twofa_alert.textContent("i18n.collectors.all.password.error");
                 }
+
+                // Stay connected if displayed
+                await driver.leftClick(MicrosoftOauth2Selectors.BUTTON_STAY_CONNECTED, { timeout: 1000, raiseException: false });
             }
+
+            /*const phoneOtpMethod = await driver.getElement(MicrosoftOauth2Selectors.BUTTON_PHONE_OTP_METHOD, { timeout: 100, raiseException: false });
+            if(phoneOtpMethod) {
+                await driver.leftClick(MicrosoftOauth2Selectors.BUTTON_PHONE_OTP_METHOD, {navigation: false});
+            }*/
+
+            throw new Error("Unknown authentication methods, unable to continue login process");
         }
     }
 
     static async needTwofa(driver: Driver): Promise<string | void> {
         if(MicrosoftOauth2.check(driver)) {
             // Click send notification button
-            await driver.leftClick(MicrosoftOauth2Selectors.BUTTON_SEND_NOTIFICATION, { navigation: false });
-
-            // Get 2FA instructions
-            const texts = await driver.getAttributes(MicrosoftOauth2Selectors.CONTAINERS_2FA_INSTRUCTIONS, "textContent");
-            return texts.join(". ");
+            const sendNotification = await driver.leftClick(MicrosoftOauth2Selectors.BUTTON_SEND_NOTIFICATION, { raiseException: false, timeout: 5000, navigation: false });
+            if(sendNotification) {
+                // Get 2FA instructions
+                const texts = await driver.getAttributes(MicrosoftOauth2Selectors.CONTAINERS_2FA_INSTRUCTIONS, "textContent");
+                return texts.join(". ");
+            }
         }
     }
 
