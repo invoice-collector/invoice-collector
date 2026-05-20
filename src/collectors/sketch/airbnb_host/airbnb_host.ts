47435623b6fe494c05c54c35e2f9101a98a62576
LucBerge <lucas.bergeron@outlook.fr>
2026-05-20 17:31:50 +0200

    Add `airbnb_host` as sketch

---
+++ src/collectors/sketch/airbnb_host/airbnb_host.ts
@@ -0,0 +1,43 @@
+import { CollectorCaptcha, CollectorState, CollectorType } from '../../abstractCollector';
+import { Driver } from '../../../driver/driver';
+import { SketchCollector } from '../../sketchCollector';
+
+export class AirbnbHostCollector extends SketchCollector {
+
+    static CONFIG = {
+        id: "airbnb_host",
+        name: "i18n.collectors.airbnb_host.name",
+        description: "i18n.collectors.airbnb_host.description",
+        version: "0",
+        website: "https://www.airbnb.com/",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/6/69/Airbnb_Logo_B%C3%A9lo.svg",
+        type: CollectorType.SKETCH,
+        params: {
+            email: {
+                type: "email",
+                name: "i18n.collectors.all.email",
+                placeholder: "i18n.collectors.all.email.placeholder",
+                mandatory: true
+            },
+            password: {
+                type: "password",
+                name: "i18n.collectors.all.password",
+                placeholder: "i18n.collectors.all.password.placeholder",
+                mandatory: true
+            }
+        },
+        loginUrl: "https://www.airbnb.com/login?redirect_url=%2Fusers%2Ftransaction_history",
+        entryUrl: "https://www.airbnb.com/users/transaction_history",
+        captcha: CollectorCaptcha.NONE,
+        enableInteractiveLogin: true,
+        state: CollectorState.PLANNED
+    }
+
+    constructor() {
+        super(AirbnbHostCollector.CONFIG);
+    }
+    
+    async needLogin(driver: Driver): Promise<boolean> {
+        return this.config.entryUrl == undefined || driver.url().includes("/login");
+    }
+}
