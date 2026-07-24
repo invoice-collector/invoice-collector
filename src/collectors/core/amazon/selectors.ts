7a0f9213cdfb1be6120837fb6a073818f5503af2
LucBerge <lucas.bergeron@outlook.fr>
2026-07-24 09:30:34 +0200

    Update collector `amazon`, bump version

--- src/collectors/core/amazon/selectors.ts
+++ src/collectors/core/amazon/selectors.ts
@@ -6,7 +6,7 @@ export const AmazonSelectors = {
         info: "select loggedin account"
     },
     CONTAINER_PERSONAL_ACCOUNT: {
-        selector: "div[data-test-id='switchableAccounts'] > div > a:has(img.cvf-account-switcher-profile-image)",
+        selector: "div.cvf-widget-form-account-switcher .a-image-container.cvf-account-switcher-personal-account-icon",
         info: "select personal account"
     },
 
